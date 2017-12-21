241db236149e68692abbd8c814249afb1ce11b11
Andrew Frank <andrew.m.frank@gmail.com>
2017-12-21 11:31:02 -0500

    started writing python code

--- analyses/PhaseCheck/src/phasedalign-to-vcf.py
+++ analyses/PhaseCheck/src/phasedalign-to-vcf.py
@@ -17,3 +17,16 @@
 #   elif    c1[i] = 1 && c2[i] = 1 && c3[i] = 0,    then phase = "1|1" & alt = allele1[i]
 #   elif    c1[i] = 1 && c2[i] = 1 && c3[i] = 1,    then phase = "1|2" & alt = allele1[i],allele2[i]
 # write to columns
+
+from Bio import SeqIO
+import os
+
+input_path = "/Users/Andrew/Drive/Work/Projects/plestiodon-anchored-phylogenomics/data/original_seqs_byIndividual"
+filename = "I7487_seq1.fasta"
+
+full_path = os.path.join(input_path,filename)
+
+# Maybe just read the whole set of 3 sequences into memory?
+
+#for index, record in enumerate(SeqIO.parse(full_path,"fasta")):
+#    print(record.seq)
