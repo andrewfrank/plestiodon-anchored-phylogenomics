153a500b364b2f014dd34d4f597b2d017dc0d340
Andrew Frank <andrew.m.frank@gmail.com>
2018-01-03 16:33:45 -0500

    vastly improved for loops, script now functions

--- analyses/PhaseCheck/src/phasedalign-to-vcf.py
+++ analyses/PhaseCheck/src/phasedalign-to-vcf.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 
-# Planned script
-# input
-#   read ref fasta
-#   read allele 1 fasta
-#   read allele 2 fasta
-# check if length(ref) = length(allele1) = length(allele2)
-# run sequence comparisons
-#   if ref[j] = allele1[j]      then c1[j] = 0, else c1[j] = 1
-#   if ref[j] = allele2[j]      then c2[j] = 0, else c2[j] = 1
-#   if allele1[j] = allele2[j]  then c3[j] = 0, else c3[j] = 1
-# determine phase notation
-#   if      c1[j] = 0 && c2[j] = 0,                 then phase = "0|0" & alt = ref[j]
-#   elif    c1[j] = 0 && c2[j] = 1,                 then phase = "0|1" & alt = allele2[j]
-#   elif    c1[j] = 1 && c2[j] = 0,                 then phase = "1|0" & alt = allele1[j]
-#   elif    c1[j] = 1 && c2[j] = 1 && c3[j] = 0,    then phase = "1|1" & alt = allele1[j]
-#   elif    c1[j] = 1 && c2[j] = 1 && c3[j] = 1,    then phase = "1|2" & alt = allele1[j],allele2[j]
-# write to columns
-
 from Bio import SeqIO
 import os
 
@@ -30,77 +12,43 @@ al1_file = os.path.join(working_dir,"original_seqs_byIndividual",sample + "_seq1
 al2_file = os.path.join(working_dir,"original_seqs_byIndividual",sample + "_seq2.fasta")
 out_file = os.path.join(working_dir,"original_vcfs",sample + "_original.vcf")
 
-ref = list(SeqIO.parse(ref_file, "fasta"))
-al1 = list(SeqIO.parse(al1_file, "fasta"))
-al2 = list(SeqIO.parse(al2_file, "fasta"))
+refs = list(SeqIO.parse(ref_file, "fasta"))
+al1s = list(SeqIO.parse(al1_file, "fasta"))
+al2s = list(SeqIO.parse(al2_file, "fasta"))
 
-chrom = []
-pos = []
-vcf_id = []
-vcf_ref = []
-alt = []
-qual = []
-vcf_filter = []
-info = []
-vcf_format = []
-ind = []
-
-# Start for loop here over each locus
-
-for i in range( len(ref) - 1):
-
-    for j in range( len(ref[i].seq) - 1 ):
-        chrom.append(ref[i].id)
-        pos.append(j + 1)
-        vcf_id.append(".")
-        vcf_ref.append(ref[i].seq[j])
-        qual.append(".")
-        vcf_filter.append(".")
-        info.append(".")
-        vcf_format.append("GT,PGT")
-
-    # ternary operators:
-    # https://www.pythoncentral.io/one-line-if-statement-in-python-ternary-conditional-operator/
-
-    c1 = []
-    c2 = []
-    c3 = []
-
-    for j in range( len(ref[i].seq) - 1 ):
-        c1.append(0 if ref[i].seq[j] == al1[i].seq[j] else 1)
-        c2.append(0 if ref[i].seq[j] == al2[i].seq[j] else 1)
-        c3.append(0 if al1[i].seq[j] == al2[i].seq[j] else 1)
-
-    for j in range( len(ref[i].seq) - 1 ):
-        if c1[j] == 0 and c2[j] == 0:
-            alt.append(ref[i].seq[j])
-            ind.append("0/0:0|0")
-        if c1[j] == 0 and c2[j] == 1:
-            alt.append(al2[i].seq[j])
-            ind.append("0/1:0|1")
-        if c1[j] == 1 and c2[j] == 0:
-            alt.append(al1[i].seq[j])
-            ind.append("0/1:1|0")
-        if c1[j] == 1 and c2[j] == 1 and c3[j] == 0:
-            alt.append(al1[i].seq[j])
-            ind.append("1/1:1|1")
-        if c1[j] == 1 and c2[j] == 1 and c3[j] == 1:
-            alt.append(al1[i].seq[j] + "," + al2[i].seq[j])
-            ind.append("1/2:1|2")
-
-# Separate loop here
 lines = ["#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t" + sample + "\n"]
-for i in range( len(chrom) - 1):
-    lines.append(chrom[i] + "\t" +
-                str(pos[i]) + "\t" +
-                vcf_id[i] + "\t" +
-                vcf_ref[i] + "\t" +
-                alt[i] + "\t" +
-                qual[i] + "\t" +
-                vcf_filter[i] + "\t" +
-                info[i] + "\t" +
-                vcf_format[i] + "\t" +
-                ind[i] + "\n")
+
+for ref, al1, al2 in zip(refs, al1s, al2s):
+
+    for i, (ref_base, al1_base, al2_base) in enumerate(zip(ref.seq, al1.seq, al2.seq)):
+
+        chrom = ref.id
+        pos = i + 1
+        vcf_id = "."
+        vcf_ref = ref_base
+        qual = "."
+        vcf_filter = "."
+        info = "."
+        vcf_format = "GT,PGT"
+
+        if ref_base == al1_base and ref_base == al2_base:
+            alt = ref_base
+            ind = "0/0:0|0"
+        if ref_base == al1_base and ref_base != al2_base:
+            alt = al2_base
+            ind = "0/1:0|1"
+        if ref_base != al1_base and ref_base == al2_base:
+            alt = al1_base
+            ind = "0/1:1|0"
+        if ref_base != al1_base and ref_base != al2_base and al1_base == al2_base:
+            alt = al1_base
+            ind = "1/1:1|1"
+        if ref_base != al1_base and ref_base != al2_base and al1_base != al2_base:
+            alt = al1_base + "," + al2_base
+            ind = "1/2:1|2"
+
+        line = chrom + "\t" + str(pos) + "\t" + vcf_id + "\t" + vcf_ref + "\t" + alt + "\t" + qual + "\t" + vcf_filter + "\t" + info + "\t" + vcf_format + "\t" + ind + "\n"
+        lines.append(line)
 
 vcf_out = open(out_file,"w")
 vcf_out.writelines(lines)
